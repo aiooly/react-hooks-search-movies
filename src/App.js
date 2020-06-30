c3cafc985285904a35e7034f648f041432ba67e7
Li Yan <yxali@thoughtworks.com>
2020-06-30 23:23:26 +0800

    Use class component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Container, Grid } from '@material-ui/core'
 import './App.css';
+import MovieSuggest from './components/MovieSuggest/View';
+import SimilarMovies from './components/SimilarMovies/View';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    selectedMovieId: null,
+  };
+
+  handleMovieSelect = movieId => {
+    this.setState({ selectedMovieId: movieId });
+  };
+
+  render() {
+    return (
+      <Container maxWidth="xl" className='App'>
+        <Grid container spacing={5}>
+          <Grid item xs>
+            <MovieSuggest onMovieSelect={this.handleMovieSelect}></MovieSuggest>
+          </Grid>
+        </Grid>
+        <Grid container spacing={5}>
+          <Grid item xs>
+            {
+              this.state.selectedMovieId && <SimilarMovies movieId={this.state.selectedMovieId}></SimilarMovies>
+            }
+          </Grid>
+        </Grid>
+      </Container>
+    );
+  }
 }
 
 export default App;
